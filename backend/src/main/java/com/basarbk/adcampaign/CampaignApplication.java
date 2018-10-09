1b4e39009a6c3c2489eaaa2b72c176b2071f690e
basarbk <basarbk@gmail.com>
2018-10-09 21:00:32 +0300

    backend and frontend base projects are created

---
+++ backend/src/main/java/com/basarbk/adcampaign/CampaignApplication.java
@@ -0,0 +1,12 @@
+package com.basarbk.adcampaign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignApplication.class, args);
+	}
+}
